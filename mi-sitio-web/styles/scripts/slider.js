b0f0be655efe46fc8e41c2cd1d12e5ce00e7d90d
mauriciopportunities <130375008+mauriciopportunities@users.noreply.github.com>
2024-06-11 12:23:30 -0600

    Add files via upload

---
+++ mi-sitio-web/styles/scripts/slider.js
@@ -0,0 +1,18 @@
+let slides = document.querySelectorAll('.slide');
+let currentSlide = 0;
+
+function showSlide(index) {
+    slides.forEach((slide, i) => {
+        slide.classList.remove('active');
+        if (i === index) {
+            slide.classList.add('active');
+        }
+    });
+}
+
+function nextSlide() {
+    currentSlide = (currentSlide + 1) % slides.length;
+    showSlide(currentSlide);
+}
+
+setInterval(nextSlide, 3000);
